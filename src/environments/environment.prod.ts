f01c3c26269bf408ff0dec554c5435df16d84fba
Diego Larrea <larreadi@personal.com.py>
2025-10-11 14:31:57 -0300

    feat: aumento de tag

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     apiUrl: 'https://s.personal.com.py/tf/api',
     appName: 'TalentFlow',
-    appVersion: 'v1.0.0'
+    appVersion: 'v1.0.2'
 };
